759922bf87c234777dbff002448353dcfa2e17cc
Cngo2019 <canngo215@gmail.com>
2022-12-27 12:48:34 -0500

    add a function to initialize grid

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const body = document.getElementById("container");
-console.log(body);
-for (let i = 0; i <= 16*16; i++) {
-    const divBlock = document.createElement("div");
-    divBlock.innerHTML = "generated from JS";
-    divBlock.className = "inner";
-    body.appendChild(divBlock);
+function changeGrid(numGrids) {
+    const body = document.getElementById("container");
+    body.style.setProperty('grid-template-columns', `repeat(${numGrids}, 1fr)`);
+    body.style.setProperty('grid-template-rows', `repeat(${numGrids}, 1fr)`);
+    console.log(body);
+    for (let i = 0; i <= 16 * 16; i++) {
+        const divBlock = document.createElement("div");
+        divBlock.innerHTML = "";
+        divBlock.className = "inner";
+        body.appendChild(divBlock);
+    }
 }
-    
