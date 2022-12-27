0c97529cf488a339f0e16d737e0cd045275322cd
Cngo2019 <canngo215@gmail.com>
2022-12-27 13:02:30 -0500

    implement function to color cell

--- script.js
+++ script.js
@@ -7,14 +7,20 @@ function changeGrid(numCells) {
         const divBlock = document.createElement("div");
         divBlock.innerHTML = "";
         divBlock.className = "inner";
+        addEventListener('hover', colorCell(e));
         body.appendChild(divBlock);
     }
+
 }
 
 // This function simply removes the container's divs
-function removeDivs(bodyNode) {
+function removeCells(bodyNode) {
     while (bodyNode.hasChildNodes()) {
         bodyNode.removeChild(bodyNode.lastChild);
     }
+}
+
+function colorCell(e) {
+    e.target.style.backgroundColor = 'red';
 }
 changeGrid(16);
