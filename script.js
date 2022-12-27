d7295d888d10ff0ea91c7363d37837b0bd8faebc
Cngo2019 <canngo215@gmail.com>
2022-12-27 13:22:59 -0500

    color cell when mouse is hovering

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ function changeGrid(numCells) {
     body.style.setProperty('grid-template-rows', `repeat(${numCells}, 1fr)`);
     console.log(body);
     for (let i = 0; i <= numCells * numCells; i++) {
-        const divBlock = document.createElement("div");
-        divBlock.innerHTML = "";
-        divBlock.className = "inner";
-        addEventListener('hover', colorCell(e));
+        const divBlock = createCell();
+        divBlock.addEventListener('mouseover', (e) => {colorCell(e)});
         body.appendChild(divBlock);
     }
 
 }
 
+function createCell() {
+    const divBlock = document.createElement("div");
+    divBlock.className = "inner";
+    divBlock.innerHTML = "";
+    return divBlock;
+}
 // This function simply removes the container's divs
 function removeCells(bodyNode) {
     while (bodyNode.hasChildNodes()) {
