eccf63b59b0953922a7533ea5fec2419eb9c74fa
Cngo2019 <canngo215@gmail.com>
2022-12-27 12:58:39 -0500

    remove div nodes implemented

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ function changeGrid(numCells) {
     body.style.setProperty('grid-template-columns', `repeat(${numCells}, 1fr)`);
     body.style.setProperty('grid-template-rows', `repeat(${numCells}, 1fr)`);
     console.log(body);
-    for (let i = 0; i <= numCells; i++) {
+    for (let i = 0; i <= numCells * numCells; i++) {
         const divBlock = document.createElement("div");
         divBlock.innerHTML = "";
         divBlock.className = "inner";
         body.appendChild(divBlock);
     }
 }
+
+// This function simply removes the container's divs
+function removeDivs(bodyNode) {
+    while (bodyNode.hasChildNodes()) {
+        bodyNode.removeChild(bodyNode.lastChild);
+    }
+}
+changeGrid(16);
