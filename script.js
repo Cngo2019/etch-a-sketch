7e8d30f54d3597ba15f83d3f4712cd86f8719b6c
Cngo2019 <canngo215@gmail.com>
2022-12-27 12:49:20 -0500

    change variable name in function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function changeGrid(numGrids) {
+function changeGrid(numCells) {
     const body = document.getElementById("container");
-    body.style.setProperty('grid-template-columns', `repeat(${numGrids}, 1fr)`);
-    body.style.setProperty('grid-template-rows', `repeat(${numGrids}, 1fr)`);
+    body.style.setProperty('grid-template-columns', `repeat(${numCells}, 1fr)`);
+    body.style.setProperty('grid-template-rows', `repeat(${numCells}, 1fr)`);
     console.log(body);
-    for (let i = 0; i <= 16 * 16; i++) {
+    for (let i = 0; i <= numCells; i++) {
         const divBlock = document.createElement("div");
         divBlock.innerHTML = "";
         divBlock.className = "inner";
